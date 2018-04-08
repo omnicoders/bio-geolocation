b213488349ad853d48d3ab97b36b9867c83d1249
3d-printer <sudoer@3d-printer>
2018-04-08 00:39:26 -0700

    Adding fasta test

--- fasta.js
+++ fasta.js
@@ -1,10 +1,41 @@
 'use strict';
 
+const fs = require('fs');
+const axios = require('axios');
+
+// eval(pry.it);
+const pry = require('pryjs');
+
 /**
  * Parses fasta files
- * @param {string} filename
+ * @param {string} srcFilePath
+ * @param {string} topic
  * @return {array}
  */
-module.exports = function(filename) {
-    return [];
+module.exports = function(srcFilePath) {
+
+    const srcFile = fs.readFileSync(srcFilePath, "utf8");
+    
+    let jsonSrcArray = [];
+	let srcArray = srcFile.split('>');
+
+    for(let i = 0; i < srcArray.length; i++){
+		let record = srcArray[i];
+		if(record.length > 10){
+			let recordArray = record.split('.1');
+			let assession = recordArray[0].trim();
+			let description = recordArray[1];
+			if(description && description.length > 0){
+				let descriptionArray = description.split(' ');
+				let name = `${descriptionArray[1]} ${descriptionArray[2]}`;
+				let jsonObj = {
+					assession: assession,
+					name: name
+				};
+				jsonSrcArray.push(jsonObj);
+			}
+		}
+    }
+
+	return jsonSrcArray;
 };
--- test/fasta-test.js
+++ test/fasta-test.js
@@ -3,10 +3,15 @@
 var expect = require('chai').expect;
 var fastaParser = require('../fasta.js');
 
+const pry = require('pryjs');
+
 describe('#fastaParser', function() {
     it('should convert fasta files', function() {
-        var result = fastaParser('./fasta/suillus.fas');
-        //expect(result).to.equal();
+        var result = fastaParser('./examples/singlesample.fas');
+        expect(result[0]['name']).to.equal('Suillus variegatus');
+        expect(result[0]['assession']).to.equal('JQ711926');
         expect(result).to.be.a('array');
+        expect(result).to.have.lengthOf(1);
+        expect(result[0]).to.be.a('object');
     });
 });
